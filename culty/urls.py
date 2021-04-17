449ba7ac49cc9227f121bc4a14d15d680f362218
waywho <walzerfan@yahoo.com>
2021-04-17 19:39:07 +0200

    lesson 17

--- culty/urls.py
+++ culty/urls.py
@@ -14,11 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', views.about),
+    path('articles/', include('articles.urls')),
     path('', views.homepage)
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- culty/views.py
+++ culty/views.py
@@ -5,6 +5,6 @@ def homepage(request):
 	# return HttpResponse('home')
 	return render(request, 'homepage.html')
 
-def about(request):
+def about(request, slug):
 	# return HttpResponse('about')
 	return render(request, 'about.html')
